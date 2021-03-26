f14b7425f7918595fe17d95c8ec53013edc3bbfc
Amirhosein Jafari Barani <amirhoseinjaafari@hotmail.com>
2021-03-26 10:41:44 +0430

    changed input structure

--- gsi.go
+++ gsi.go
@@ -7,8 +7,8 @@ import (
 )
 
 //function Input Returning user input as string
-func Input(message string) string {
-	fmt.Printf("%s", message)
+func Input(a ...interface{}) string {
+	fmt.Printf("%s", a...)
 	scanner := bufio.NewScanner(os.Stdin)
 	scanner.Scan()
 	return scanner.Text()
