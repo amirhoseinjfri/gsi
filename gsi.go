f68a7207667da021b460a1a7a04369e624b97bdc
Amirhosein Jafari Barani <amirhoseinjaafari@hotmail.com>
2021-03-26 10:58:58 +0430

    fixed Beep Sound

--- gsi.go
+++ gsi.go
@@ -8,11 +8,11 @@ import (
 
 //function Input Returning user input as string
 func Input(s bool, a ...interface{}) string {
+	if s {
+		fmt.Printf("\a")
+	}
 	fmt.Printf("%v", a...)
 	scanner := bufio.NewScanner(os.Stdin)
 	scanner.Scan()
 	return scanner.Text()
-	if s == true {
-		fmt.Printf("\a")
-	}
 }
