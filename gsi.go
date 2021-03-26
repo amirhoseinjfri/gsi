63ec4f5f93f48d07f6226ac5d827868000cb7abc
Amirhosein Jafari Barani <amirhoseinjaafari@hotmail.com>
2021-03-26 10:54:04 +0430

    added beep sound

--- gsi.go
+++ gsi.go
@@ -7,9 +7,12 @@ import (
 )
 
 //function Input Returning user input as string
-func Input(a ...interface{}) string {
+func Input(s bool, a ...interface{}) string {
 	fmt.Printf("%v", a...)
 	scanner := bufio.NewScanner(os.Stdin)
 	scanner.Scan()
 	return scanner.Text()
+	if s == true {
+		fmt.Printf("\a")
+	}
 }
